9915fea128f71277b40581f9944432dfb9fdb7fb
Francis Genet <58907@supinfo.com>
2013-03-22 11:46:27 -0700

    first commit

---
+++ numbers.php
@@ -0,0 +1,19 @@
+<?php 
+
+/**
+ * Numbers management class
+ *
+ * @author Francis Genet
+ * @package Bandwidth-manager
+ * @version 1.0
+ */
+
+class Numbers {
+    /**
+     * will return an object with the info on the phone registration
+     *
+     * @url POST /site/{site_name}
+     */
+}
+
+ ?>
