31e3f0fcb0fdd71d35cac4bdaa97e580d6265726
frifri <francis.genet@gmail.com>
2013-04-04 17:12:43 -0700

    Add first draft structure for api

---
+++ api/index.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This is the entry point for the APIs
+ *
+ * @author Francis Genet
+ * @package Bandwidth-manager
+ * @version 1.0
+ */
+
+require_once 'helper/settings.php';
+require_once 'lib/restler/restler.php';
+
+use Luracast\Restler\Restler;
+
+// CORS
+header('Access-Control-Allow-Headers:Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, X-Auth-Token');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Max-Age:86400');
+
+$r = new Restler();
+$r->setSupportedFormats('JsonFormat');
+//$r->addAuthenticationClass('AccessControl');
+$r->handle();
+
+?>
