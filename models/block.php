fa9a370fa043ab79001262f25f00870da015fc76
frifri <francis.genet@gmail.com>
2013-04-09 15:48:52 -0700

    Adding number and block models

---
+++ models/block.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+ * Block model
+ * @author Francis Genet
+ * @package Number_manager
+ */
+class models_block {
+    function __construct() {
+        
+    }
+}
+    
+ ?>
---
+++ models/number.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Number model
+ * @author Francis Genet
+ * @package Number_manager
+ */
+class models_number {
+    private $_id;
+    private $_number;
+    private $_provider;
+    private $_cache_date;
+    private $_last_update;
+    private $_city;
+    private $_state;
+
+    function __construct() {
+        # code...
+    }
+
+}
+
+ ?>
