810f7ce07ff2bd76a4c27c534b346ebb1a85971c
frifri <francis.genet@gmail.com>
2013-04-09 17:18:57 -0700

    Update bandwidth provider to use the models

--- providers/bandwidth/provider.php
+++ providers/bandwidth/provider.php
@@ -2,30 +2,18 @@
 
 class providers_bandwidth_provider implements providers_iprovider {
     private $_curl;
-    private $_provider_settings;
-    private $_database_settings;
+    private $_settings;
     private $_db;
 
     function __construct() {
         $general_settings = helper_settings::get_instance();
-        $this->_provider_settings = $general_settings->providers->{ENVIRONMENT}->bandwidth;
-        $this->_database_settings = $general_settings->database;
+        $this->_settings = $general_settings->providers->{ENVIRONMENT}->bandwidth;
         $this->_init_curl();
-        $this->_init_mysql();
     }
 
     function __destruct() {
         curl_close($this->_curl);
         $this->_db = null;
-    }
-
-    private function _init_mysql() {
-        // Set the DSN (the string that determines what driver to user and how)
-        $dsn = "mysql:host=" . $this->_database_settings->database_host . ";dbname=" . $this->_database_settings->database_name . ";charset=" . $this->_database_settings->database_charset;
-        // Set the driver parameters
-        $drvr_params = array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-        // Creating a connexion
-        $this->_db = new PDO($dsn, $this->_database_settings->database_user, $this->_database_settings->database_password, $drvr_params);
     }
 
     // Initializing curl with common params
@@ -39,7 +27,7 @@ class providers_bandwidth_provider implements providers_iprovider {
             CURLOPT_SSL_VERIFYPEER => true,
             CURLOPT_SSL_VERIFYHOST => 2,
             CURLOPT_CAINFO => ROOT_PATH . "/certs/apitest.crt",
-            CURLOPT_USERPWD => $this->_provider_settings->username . ":" . $this->_provider_settings->password
+            CURLOPT_USERPWD => $this->_settings->username . ":" . $this->_settings->password
         ));
     }
 
@@ -47,7 +35,7 @@ class providers_bandwidth_provider implements providers_iprovider {
         if (!$area_code)
             die("Empty area code\n");
 
-        $url = $this->_provider_settings->api_url . "accounts/" . $this->_provider_settings->account_id . "/availableNpaNxx?&areaCode=" . $area_code;
+        $url = $this->_settings->api_url . "accounts/" . $this->_settings->account_id . "/availableNpaNxx?&areaCode=" . $area_code;
 
         curl_setopt_array($this->_curl, array(
             CURLOPT_CUSTOMREQUEST => "GET",
@@ -62,7 +50,7 @@ class providers_bandwidth_provider implements providers_iprovider {
             $state = $result->State;
             $npanxx = $result->Npa . $result->Nxx;
 
-            $url = $this->_provider_settings->api_url . "accounts/" . $this->_provider_settings->account_id . "/availableNumbers?&npaNxx=" . $npanxx;
+            $url = $this->_settings->api_url . "accounts/" . $this->_settings->account_id . "/availableNumbers?&npaNxx=" . $npanxx;
 
             curl_setopt_array($this->_curl, array(
                 CURLOPT_URL => $url
@@ -70,20 +58,18 @@ class providers_bandwidth_provider implements providers_iprovider {
 
             $xml_number_result = simplexml_load_string(curl_exec($this->_curl));
 
-            $group_size = 0;
-            $previous_number = null;
+            // Creating first block
+            $obj_block = new models_block("bandwidth");
             foreach ($xml_number_result->TelephoneNumberList->TelephoneNumber as $number) {
-                // Adding number
-                try {
-                    $stmt_number = $this->_db->prepare("INSERT INTO `numbers`(`number`, `provider`, `cache_update`, `city`, `state`) VALUES(?, ?, now(), ?, ?)");
-                    $stmt_number->execute(array($number, "bandwidth", $city, $state));
-                } catch (PDOException $e) {
-                    echo($e->getMessage() . "\n");
-                }
+                // Creating number model
+                $obj_number = new models_number("bandwidth");
+                $obj_number->set_number($number);
+                $obj_number->set_city($city);
+                $obj_number->set_state($state);
+                $obj_number->insert();
 
-                // Adding block
+
             }
-
             die();
         }
     }
