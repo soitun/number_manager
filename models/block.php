4f8453b211f049d1dc91887cce5181cc007d0bf1
frifri <francis.genet@gmail.com>
2013-04-09 17:18:24 -0700

    Update number and block models

--- models/block.php
+++ models/block.php
@@ -5,9 +5,48 @@
  * @author Francis Genet
  * @package Number_manager
  */
-class models_block {
-    function __construct() {
-        
+class models_block extends models_model {
+    private $_id;
+    private $_size;
+    private $_start_number;
+    private $_end_number;
+
+    // === Setter ===
+
+    public function set_size($size) {
+        $this->_size = $size;
+    }
+
+    public function set_start_number($start_number) {
+        $this->_start_number = $start_number;
+    }
+
+    public function set_end_number($end_number) {
+        $this->_end_number = $end_number;
+    }
+
+    // === Getter ===
+
+    public function get_size($size) {
+        return $this->_size;
+    }
+
+    public function get_start_number($start_number) {
+        return $this->_start_number;
+    }
+
+    public function get_end_number($end_number) {
+        return $this->_end_number;
+    }
+
+    // ==============
+
+    function __construct($provider) {
+        parent::__construct($provider);
+    }
+
+    function __destruct() {
+        parent::__destruct();
     }
 }
     
--- models/number.php
+++ models/number.php
@@ -5,19 +5,77 @@
  * @author Francis Genet
  * @package Number_manager
  */
-class models_number {
+class models_number extends models_model {
     private $_id;
     private $_number;
-    private $_provider;
     private $_cache_date;
     private $_last_update;
     private $_city;
     private $_state;
 
-    function __construct() {
-        # code...
+    // === Setter ===
+
+    public function set_number($number) {
+        $this->_number = $number;
     }
 
+    public function set_cache_date($cache_date) {
+        $this->_cache_date = $cache_date;
+    }
+
+    public function set_last_update($last_update) {
+        $this->_last_update = $last_update;
+    }
+
+    public function set_city($city) {
+        $this->_city = $city;
+    }
+
+    public function set_state($state) {
+        $this->_state = $state;
+    }
+
+    // === Getter ===
+
+    public function get_number($number) {
+        return $this->_number;
+    }
+
+    public function get_cache_date($cache_date) {
+        return $this->_cache_date;
+    }
+
+    public function get_last_update($last_update) {
+        return $this->_last_update;
+    }
+
+    public function get_city($city) {
+        return $this->_city;
+    }
+
+    public function get_state($state) {
+        return $this->_state;
+    }
+
+    // ==============
+
+    function __construct($provider) {
+        parent::__construct($provider);
+    }
+
+    function __destruct() {
+        parent::__destruct();
+    }
+
+    // Adding number in DB
+    public function insert() {
+        try {
+            $stmt = $this->_db->prepare("INSERT INTO `numbers`(`number`, `provider`, `cache_update`, `city`, `state`) VALUES(?, ?, now(), ?, ?)");
+            $stmt->execute(array($this->_number, $this->_provider, $this->_city, $this->_state));
+        } catch (PDOException $e) {
+            echo($e->getMessage() . "\n");
+        }
+    }
 }
 
  ?>
