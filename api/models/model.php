6efe269d4b743509e6f25c39e21daed0f15542e1
frifri <francis.genet@gmail.com>
2013-04-25 14:06:24 -0700

    Model return value

--- api/models/model.php
+++ api/models/model.php
@@ -12,7 +12,9 @@ class models_model {
 
     function __construct() {
         $this->_settings = helper_settings::get_instance();
-        $this->_init_mysql();
+
+        if (!$this->_init_mysql())
+            return false;
     }
 
     function __destruct() {
@@ -21,15 +23,16 @@ class models_model {
 
     private function _init_mysql() {
         // Set the DSN (the string that determines what driver to user and how)
-        $dsn = "mysql:host=" . $this->_settings->database->database_host . ";dbname=" . $this->_settings->database->database_name . ";charset=" . $this->_settings->database->database_charset;        
+        $dsn = "mysql:host=" . $this->_settings->database->database_host . ";dbname=" . $this->_settings->database->database_name . ";charset=" . $this->_settings->database->database_charset;
         // Set the driver parameters
         $drvr_params = array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
         // Creating a connexion
-
         try {
             $this->_db = new PDO($dsn, $this->_settings->database->database_user, $this->_settings->database->database_password, $drvr_params);
+            return true;
         } catch (PDOException $e) {
-            echo $e->getMessage() . "\n";
+            //echo $e->getMessage() . "\n";
+            return false;
         }
     }
 }
