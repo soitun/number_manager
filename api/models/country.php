8a43218c2246000103a26bc707c82c1ea1303386
frifri <francis.genet@gmail.com>
2013-04-25 14:04:14 -0700

    It is now possible to load a country from a iso_code

--- api/models/country.php
+++ api/models/country.php
@@ -15,12 +15,42 @@ class models_country extends models_model {
     private $_flag_url;
     private $_name;
 
-    function __construct() {
+    function __construct($iso_code = null) {
         parent::__construct();
+
+        if ($iso_code) {
+            try {
+                $query = "SELECT * FROM `countries` WHERE `iso_code` = ?";
+                $stmt = $this->_db->prepare($query);
+                $stmt->execute(array($iso_code));
+
+                if ($stmt->rowCount()) {
+                    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    $this->_id = $result[0]['id'];
+                    $this->_iso_code = $result[0]['iso_code'];
+                    $this->_local = $result[0]['local'];
+                    $this->_toll_free = explode(' ', $result[0]['toll_free']);
+                    $this->_vanity = $result[0]['vanity'];
+                    $this->_prefix = $result[0]['prefix'];
+                    $this->_flag_url = $result[0]['flag_url'];
+                    $this->_name = $result[0]['name'];
+
+                    return true;
+                } else 
+                    return false;
+            } catch (PDOException $e) {
+                //echo $e->get_message();
+                return false;
+            }
+        }
     }
 
     function __destruct() {
         parent::__destruct();
+    }
+
+    public function get_prefix() {
+        return $this->_prefix;
     }
 
     public function get_countries() {
