f8a13d197d7f8f636a670bf87f3cf529c4180e61
frifri <francis.genet@gmail.com>
2013-07-25 16:41:57 -0700

    Adding assets folder to bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -15,6 +15,7 @@ define("ROOT_PATH", dirname(__FILE__) . '/');
 define("CERTS_PATH", ROOT_PATH . 'certs/');
 define("LIB_PATH", ROOT_PATH . 'lib/');
 define("LOGS_PATH", ROOT_PATH . 'logs/');
+define("ASSETS_PATH", ROOT_PATH . 'assets/');
 
 // Include our auto-loader
 require_once(ROOT_PATH . 'autoload.php');
