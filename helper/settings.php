6fd47b4dd575b9314ed45192482c22e262c699c7
frifri <francis.genet@gmail.com>
2013-07-25 16:46:59 -0700

    Improving the settings helper to be able to modify the file at runtime

--- helper/settings.php
+++ helper/settings.php
@@ -10,14 +10,16 @@
 
 class helper_settings {
     private $_objSettings = null;
+    private $_file;
 
-    public static function get_instance() {
-        $objSettings = new helper_settings();
+    public static function get_instance($filename = 'config.json') {
+        $objSettings = new helper_settings($filename);
         return $objSettings->get_settings();
     }
 
-    public function __construct() {
-        $arr_file_content = json_decode(file_get_contents(ROOT_PATH . 'config.json'), true);
+    public function __construct($filename = 'config.json') {
+        $this->_file = ROOT_PATH . $filename;
+        $arr_file_content = json_decode(file_get_contents($this->_file), true);
         $this->_objSettings = $this->_array_to_object($arr_file_content);
 
         $error = $this->_json_error();
@@ -31,6 +33,10 @@ class helper_settings {
         return $this->_objSettings;
     }
 
+    public function set_settings($settings) {
+        $this->_objSettings = $settings;
+    }
+
     private function _array_to_object($array) {
         $obj = new stdClass;
         foreach($array as $k => $v) {
@@ -41,6 +47,10 @@ class helper_settings {
             }
         }
         return $obj;
+    }
+
+    public function write() {
+        file_put_contents($this->_file, json_encode($this->_objSettings, JSON_PRETTY_PRINT));
     }
 
     private function _json_error() {
