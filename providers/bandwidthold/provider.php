edf7ea5aabd85b3d2e566ce6dbfa0580ea372cbc
frifri <francis.genet@gmail.com>
2014-03-05 18:11:32 -0800

    Making sure that the update is working and Adding a second check if it fails the first time

--- providers/bandwidthold/provider.php
+++ providers/bandwidthold/provider.php
@@ -116,90 +116,99 @@ class providers_bandwidthold_provider extends providers_aprovider {
         return $quantity;
     }
 
-    private function _dowload_numbers($state, $update = false) {
+    private function _number_process($rate_center, &$zerocheck, &$number_obj, $update) {
+        $numbers = array();
         $arr_numbers = array();
+        $quantity = 0;
+
+        echo "Increasing size of result list to 5000 that bandwidth.com will return to us for each request...\n";
+        $this->_make_request("https://my.bandwidth.com/portal/Members/NumberManagement/ListNumbers.aspx?RateCenterID=$rate_center", "GET");
+
+        echo "Now processing $rate_center rate center...\n";
+        $quantity = $this->_search_numbers($rate_center, $numbers);
+        echo "  Found $quantity numbers!\n";
+
+        if ($quantity != 0) {
+            if ($quantity == 500) {
+                echo "  NOTICE: This is probably not the full list. Re-running the query with an additional search parameter...\n";
+                // If you get 500 numbers back, there are more to be had! But bandwidth.com won't send them to you :(
+                // Figure out all the prefixes in the numbers list and then run a second query for each prefix to get the full list
+                $prefixes = array();
+
+                foreach ($numbers as $number => $v) {
+                    $prefixes[substr($number, 2, 5)] = true;
+                }
+                $prefixes = array_keys($prefixes);
+
+                $quantity = $this->_search_numbers($rate_center, $numbers, $prefixes);
+                echo "  Found $quantity numbers on second try!\n";
+
+                // Still returning 5000+ numbers? Make another query to split this prefix into tenths
+                // You can work around this - find all the prefixes of the numbers that were just returned and then query each prefix (i.e. if you got 4158867900 go re-run the query for 415886)
+                // If you get back 5000 numbers, you have hit yet another limit of their API. This time we need to get more aggressive - do 10 queries for 4158861, 4158862, 4158863, etc. thru 4158869
+            }
+
+            $arr_numbers = array();
+
+            // Just getting the first element and the npa that is going to be used
+            current($numbers);
+            next($numbers);
+            $current_npa = substr(str_replace('+', '', key($numbers)), 1, 3);
+
+            $number_obj->set_db_name('US_' . $current_npa);
+
+            if ($update)
+                $number_obj->delete_like_city();
+
+            foreach ($numbers as $number => $data) {
+                $number_obj->set_number($number);
+                $number_obj->set_number_identifier($data["telID"]);
+                $number_obj->insert();
+
+                $arr_numbers[] = $number;
+            }
+
+            // And finally inserting the blocks
+            $this->_insert_block($arr_numbers);
+
+            // Destroying arr_numbers... Because!
+            $arr_numbers = null;
+
+            // We need to reset zerocheck to zero to make sure it will go
+            // through the process next time
+            $zerocheck->$rate_center = 0;
+
+            return true;
+        } else {
+            $zerocheck->$rate_center = $zerocheck->$rate_center + 1;
+            return false;
+        }
+    }
+
+    private function _dowload_numbers($state, $update = false) {
         $zerocheck = $this->_zerocheck_obj->get_settings();
         $number_obj = new models_number('bandwidthold');
 
         /* STEP 1. Figure out what rate centers we're going to scan */
         $rate_centers = file(ASSETS_PATH . $this->_provider_name . '/' . $this->_settings->rc_filename . '-' . $state);
 
-        /* STEP 2. Open output file. */
-        $fp = fopen (ASSETS_PATH . $this->_provider_name . '/' . $this->_settings->nbr_filename . '-' . $state, "w+");
-
         /* STEP 3. Begin going through each rate center, one by one */
-        $get_all_rows = false;
         foreach ($rate_centers as $rate_center) {
-            $numbers = array();
-            $quantity = 0;
             $tmp = explode('|', $rate_center);
             $rate_center = trim($tmp[0]);
-            $rate_center_name = $tmp[1];
             $rate_center_city = trim(explode(',', $tmp[1])[0]);
 
             $number_obj->set_city($rate_center_city);
             $number_obj->set_state($state);
 
             if ($zerocheck->$rate_center <= $this->_settings->max_zerocheck) {
-                echo "Increasing size of result list to 5000 that bandwidth.com will return to us for each request...\n";
-                $this->_make_request("https://my.bandwidth.com/portal/Members/NumberManagement/ListNumbers.aspx?RateCenterID=$rate_center", "GET");
-
-                echo "Now processing $rate_center rate center...\n";
-                $quantity = $this->_search_numbers($rate_center, $numbers);
-                echo "  Found $quantity numbers!\n";
-
-                if ($quantity != 0) {
-                    if ($quantity == 500) {
-                        echo "  NOTICE: This is probably not the full list. Re-running the query with an additional search parameter...\n";
-                        // If you get 500 numbers back, there are more to be had! But bandwidth.com won't send them to you :(
-                        // Figure out all the prefixes in the numbers list and then run a second query for each prefix to get the full list
-                        $prefixes = array();
-
-                        foreach ($numbers as $number => $v) {
-                            $prefixes[substr($number, 2, 5)] = true;
-                        }
-                        $prefixes = array_keys($prefixes);
-
-                        $quantity = $this->_search_numbers($rate_center, $numbers, $prefixes);
-                        echo "  Found $quantity numbers on second try!\n";
-
-                        // Still returning 5000+ numbers? Make another query to split this prefix into tenths
-                        // You can work around this - find all the prefixes of the numbers that were just returned and then query each prefix (i.e. if you got 4158867900 go re-run the query for 415886)
-                        // If you get back 5000 numbers, you have hit yet another limit of their API. This time we need to get more aggressive - do 10 queries for 4158861, 4158862, 4158863, etc. thru 4158869
-                    }
-
-                    $arr_numbers = array();
-
-                    // Just getting the first element and the npa that is going to be used
-                    current($numbers);
-                    next($numbers);
-                    $current_npa = substr(str_replace('+', '', key($numbers)), 1, 3);
-
-                    $number_obj->set_db_name('US_' . $current_npa);
-
-                    if ($update)
-                        $number_obj->delete_like_city();
-
-                    foreach ($numbers as $number => $data) {
-                        fputs($fp, $number . "," . $data["RateCenterID"] . "," . $data["telID"] . "\n");
-                        $number_obj->set_number($number);
-                        $number_obj->set_number_identifier($data["telID"]);
-                        $number_obj->insert();
-
-                        $arr_numbers[] = $number;
-                    }
-
-                    // And finally inserting the blocks
-                    $this->_insert_block($arr_numbers);
-
-                    // Destroying arr_numbers... Because!
-                    $arr_numbers = null;
-
-                    // We need to reset zerocheck to zero to make sure it will go
-                    // through the process next time
-                    $zerocheck->$rate_center = 0;
-                } else
-                    $zerocheck->$rate_center = $zerocheck->$rate_center + 1;
+                echo "First attempt... \n";
+                if (!$this->_number_process($rate_center, $zerocheck, $number_obj, $update)) {
+                    echo "First attempt failed... Let's try again\n";
+                    // Yeah, it seems that on the Bandwidth, 1/2 request fails.
+                    // So just retrying... You never know
+                    $this->_number_process($rate_center, $zerocheck, $number_obj, $update);
+                }
 
                 sleep($this->_settings->wait_timer);
             }
@@ -208,8 +217,6 @@ class providers_bandwidthold_provider extends providers_aprovider {
         // Save zerocheck
         $this->_zerocheck_obj->set_settings($zerocheck);
         $this->_zerocheck_obj->write();
-
-        fclose($fp);
     }
 
     public function create() {
